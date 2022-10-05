743d4a70180ca73dcff0e6b660967e09109ab1c0
Munkh-Batchuluun <munkh.bc@gmail.com>
2022-10-05 20:44:17 +1100

    added grid view in home page

---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+
+
+function Home () {
+    return(
+
+
+        <div className="grid-container">
+            <div className="logo-container">
+                <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+            </div>
+            <div className="logo-container">
+                <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+            </div>
+            <div className="logo-container">
+                <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+            </div>
+            <div className="logo-container">
+                <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+            </div>
+            <div className="logo-container">
+                <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+            </div>
+            <div className="logo-container">
+                <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+            </div>
+            <div className="logo-container">
+                <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+            </div>
+        </div>
+
+        
+    ) 
+}
+
+export default Home
