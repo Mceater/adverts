518ee3868265e0e2c41ca77a615729413b2c2c3e
Munkh-Batchuluun <munkh.bc@gmail.com>
2022-10-05 21:09:51 +1100

    added zoon and panning but it needs more work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,41 @@
-
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 function Home () {
     return(
 
+        <div className="container">
+            <TransformWrapper>
+                <TransformComponent>
+                <div className="grid-container">
 
-        <div className="grid-container">
-            <div className="logo-container">
-                <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
-            </div>
-            <div className="logo-container">
-                <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
-            </div>
-            <div className="logo-container">
-                <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
-            </div>
-            <div className="logo-container">
-                <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
-            </div>
-            <div className="logo-container">
-                <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
-            </div>
-            <div className="logo-container">
-                <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
-            </div>
-            <div className="logo-container">
-                <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
-            </div>
+                <div className="logo-container">
+                    <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                </div>
+                <div className="logo-container">
+                    <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                </div>
+                <div className="logo-container">
+                    <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+                </div>
+                <div className="logo-container">
+                    <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                </div>
+                <div className="logo-container">
+                    <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                </div>
+                <div className="logo-container">
+                    <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                </div>
+                <div className="logo-container">
+                    <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                </div>
+
+                </div>
+                </TransformComponent>
+            </TransformWrapper>
+            
         </div>
+        
 
         
     ) 
