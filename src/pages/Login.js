dfd7b2fb4b3df56f208152e6e474ff2a4a4cc146
Munkh-Batchuluun <munkh.bc@gmail.com>
2022-10-05 22:34:59 +1100

    added empty pages

---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+
+
+export default function Login (){
+    return (
+        <div>
+            <div className="header">
+                
+            </div>
+
+            <div className="footer">
+                <ul className="footer-containts">
+                    <li><a href="About-Us">About Us</a></li>
+                    <li><a href="Services">Services</a></li>
+                    <li><a href="Price">Price</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
