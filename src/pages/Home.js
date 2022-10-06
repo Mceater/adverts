4d9b77a13b1484cc289dab7290c43c5377746220
Yeomin Austin Yoon <yeomin.yoon@students.mq.edu.au>
2022-10-06 22:55:56 +1100

    open login form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,54 +1,78 @@
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import { useEffect, useState } from 'react';
-import service from '../service/Service';
+import { useEffect, useState } from "react";
+import service from "../service/Service";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "../pages/Login";
 
-function Home () {
+function Home() {
+  const [ads, setAds] = useState([]);
 
-    const [ads, setAds] = useState([])
+  const getAds = () => {
+    service.getAll().then((obj) => {
+      setAds(obj);
+    });
+  };
 
-    const getAds = () => {
-        service.getAll()
-        .then(obj => {
-            setAds(obj)
-        })
-    }
+  const navigate = useNavigate();
+  const navigateToLogin = () => {
+    navigate("/login");
+  };
 
-    useEffect(() => {
-        getAds()
-    })
+  //   const navigateHome = () => {
+  //     // 👇️ navigate to /
+  //     navigate("/");
+  //   };
 
-    return(
+  useEffect(() => {
+    getAds();
+  });
 
-        <div className="container">
-            <div className="login">
-                <input onClick={""} className="btn" type="button" value="Login"/>
-            </div>
-            <TransformWrapper>
-                <TransformComponent>
-                <div className="grid-container">
-                    {
-                        ads.map( ad => 
-                            <div key={ad.id} className="logo-container glass">
-                                <img className="ad-logo" src={"/images/"+ad.img} alt="logo1"/>
-                            </div>
-                        )
-                    }
-                </div>
-                </TransformComponent>
-            </TransformWrapper>
-            
-            <div className="footer">
-                <ul className="footer-containts">
-                    <li><a href="About-Us">About Us</a></li>
-                    <li><a href="Services">Services</a></li>
-                    <li><a href="Price">Price</a></li>
-                </ul>
-            </div>
-        </div>
-        
+  return (
+    <div className="container">
+      <div className="login">
+        <input
+          onClick={navigateToLogin}
+          className="btn"
+          type="button"
+          value="Login"
+        />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+      </div>
 
-        
-    ) 
+      <TransformWrapper>
+        <TransformComponent>
+          <div className="grid-container">
+            {ads.map((ad) => (
+              <div key={ad.id} className="logo-container glass">
+                <img
+                  className="ad-logo"
+                  src={"/images/" + ad.img}
+                  alt="logo1"
+                />
+              </div>
+            ))}
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
+
+      <div className="footer">
+        <ul className="footer-containts">
+          <li>
+            <a href="About-Us">About Us</a>
+          </li>
+          <li>
+            <a href="Services">Services</a>
+          </li>
+          <li>
+            <a href="Price">Price</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
