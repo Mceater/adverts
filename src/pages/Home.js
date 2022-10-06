c7e29963b8e55624cb0281d25fd8c65eb77983ce
Munkh-Batchuluun <munkh.bc@gmail.com>
2022-10-06 17:55:50 +1100

    tried to add glossy look to logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,27 +11,72 @@ function Home () {
                 <TransformComponent>
                 <div className="grid-container">
 
-                <div className="logo-container">
-                    <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
-                </div>
-                <div className="logo-container">
-                    <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
-                </div>
-                <div className="logo-container">
-                    <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
-                </div>
-                <div className="logo-container">
-                    <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
-                </div>
-                <div className="logo-container">
-                    <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
-                </div>
-                <div className="logo-container">
-                    <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
-                </div>
-                <div className="logo-container">
-                    <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
-                </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_2.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_3.png" alt="logo1"/>
+                    </div>
+                    <div className="logo-container glass">
+                        <img className="ad-logo" src="/images/logo_1.png" alt="logo1"/>
+                    </div>
 
                 </div>
                 </TransformComponent>
