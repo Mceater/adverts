f18be8b72e9a7630e30e74c6293d3d970e7090d4
Yeomin Austin Yoon <yeomin.yoon@students.mq.edu.au>
2022-10-20 21:45:06 +1100

    Userpage realign

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -4,13 +4,13 @@ import OriginalUrl from "./OriginalUrl";
 import ShortenUrl from "./ShotenUrl";
 import AdList from "../components/AdList";
 
-function Userpage ({data, user, isAuthenticated}) {
-    const { logout, loginWithRedirect } = useAuth0();
-    let userData = []
-    
-    if(user !== undefined){
-        userData = data.filter(obj => obj.userId === user.sub)
-    }
+function Userpage({ data, user, isAuthenticated }) {
+  const { logout, loginWithRedirect } = useAuth0();
+  let userData = [];
+
+  if (user !== undefined) {
+    userData = data.filter((obj) => obj.userId === user.sub);
+  }
   const [inputValue, setInputValue] = useState("");
 
   return (
@@ -25,42 +25,40 @@ function Userpage ({data, user, isAuthenticated}) {
           </div>
         </div>
       )}
-     
-
       {isAuthenticated && (
-
-                <div className="header">
-                    <div className="userPhoto">
-                        <img src={user.picture} alt="User"/>
-                    </div>
-                    <h1>{user.name}</h1>
-                    <div className='btn-wrapper'> 
-                        <button onClick={() => logout()} className='login-btn'>Logout</button>
-                    </div>
-
-
-                <div className='applications'>
-                        <OriginalUrl setInputValue={setInputValue} />
-                        <ShortenUrl inputValue={inputValue} />
-
-                        </div>
-
-                    <div className='form'>
-                        <h2>Your List of Ads</h2>
-                        <div className='list-of-ads'>
-                            <div className='list-header'>
-                                <h3>Logo</h3>
-                                <h3>URL links</h3>
-                                <h3>Started</h3>
-                                <h3>Expire</h3>
-                            </div>
-                            {userData.map(obj => <AdList key={obj.id} data={obj}/>)}
-                        </div>
-                    </div>
-                    </div>
-           )}
-           </div>
-
+        <div>
+          <div className="header">
+            <div className="userPhoto">
+              <img src={user.picture} alt="User" />
+            </div>
+            <h1>{user.name}</h1>
+            <div className="btn-wrapper">
+              <button onClick={() => logout()} className="login-btn">
+                Logout
+              </button>
+            </div>
+          </div>
+          <div className="applications">
+            <OriginalUrl setInputValue={setInputValue} />
+            <ShortenUrl inputValue={inputValue} />
+            <div className="form">
+              <h2>Your List of Ads</h2>
+              <div className="list-of-ads">
+                <div className="list-header">
+                  <h3>Logo</h3>
+                  <h3>URL links</h3>
+                  <h3>Started</h3>
+                  <h3>Expire</h3>
+                </div>
+                {userData.map((obj) => (
+                  <AdList key={obj.id} data={obj} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
