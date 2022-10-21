911182970c808e2a12b7a387fd2929bef6709353
sjg2020 <shane.garcia@students.mq.edu.au>
2022-10-21 11:16:04 +1100

    Update Price Page

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -1,7 +1,38 @@
+
+
 function Price () {
     return(
-     <div>TPrice
-
+     <div>
+      <div class="background1">
+  <div class="container1">
+    <div class="panel pricing-table">
+      
+      <div class="pricing-plan">
+        <img src="images/dollar.png" alt="" class="pricing-img"/>
+        <h2 class="pricing-header">Cheap-ShortTerm</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Affordable</li>
+          <li class="pricing-features-item">3 months</li>
+        </ul>
+        <span class="pricing-price">$0.01</span>
+        <a href="#/" class="pricing-button is-featured">Pay</a>
+      </div>
+      
+      <div class="pricing-plan">
+        <img src="images/doubledollar.png" alt="" class="pricing-img"/>
+        <h2 class="pricing-header">Expensive-LongTerm</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Costly</li>
+          <li class="pricing-features-item">6 months</li>
+        </ul>
+        <span class="pricing-price">$0.019</span>
+        <a href="#/" class="pricing-button is-featured">Pay</a>
+      </div>
+      
+ 
+    </div>
+  </div>
+</div>
      </div>   
     )
 }
