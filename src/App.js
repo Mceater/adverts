48dbf77dd0f2630e5e984144dc3c60bee1805000
Yeomin Austin Yoon <yeomin.yoon@students.mq.edu.au>
2022-10-28 00:47:48 +1100

    try paypal

--- src/App.js
+++ src/App.js
@@ -1,47 +1,57 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import Service from "./services/Service";
-import Home from './pages/Home'
-import AboutUs from './pages/AboutUs';
-import UserPage from './pages/UserPage';
-import Services from './pages/Services';
-import Price from './pages/Price';
-import Navbar from './components/Navbar';
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import UserPage from "./pages/UserPage";
+import Services from "./pages/Services";
+import Price from "./pages/Price";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   const { isAuthenticated, user } = useAuth0();
 
   const getData = () => {
-      Service.getAll()
-      .then( obj => {
-          setData(obj)
-      })
-  }
+    Service.getAll().then((obj) => {
+      setData(obj);
+    });
+  };
 
   useEffect(() => {
-    getData()
-  },[])
-
+    getData();
+  }, []);
 
   return (
-    <div className='container'>
-      
+    <div className="container">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home data={data} user={user} isAuthenticated={isAuthenticated}/>}/>
-          <Route path="/userpage" element={<UserPage data={data} user={user} isAuthenticated={isAuthenticated}/>}/>
-          <Route path="/about-us" element={<AboutUs/>}/>
-          <Route path="/services" element={<Services/>}/>
-          <Route path="/price" element={<Price/>}/>
+          <Route
+            path="/"
+            element={
+              <Home data={data} user={user} isAuthenticated={isAuthenticated} />
+            }
+          />
+          <Route
+            path="/userpage"
+            element={
+              <UserPage
+                data={data}
+                user={user}
+                isAuthenticated={isAuthenticated}
+              />
+            }
+          />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/price" element={<Price />} />
         </Routes>
       </Router>
     </div>
   );
 }
-
 
 export default App;
--- src/pages/Price.js
+++ src/pages/Price.js
@@ -1,40 +1,49 @@
+// import PaypalCheckoutButton from "../components/PaypalCheckoutButton";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
+function Price() {
+  return (
+    <PayPalScriptProvider
+      options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID }}
+    >
+      <div>
+        <div class="background1">
+          <div class="container1">
+            <div class="panel pricing-table">
+              <div class="pricing-plan">
+                <img src="images/dollar.png" alt="" class="pricing-img" />
+                <h2 class="pricing-header">Cheap-ShortTerm</h2>
+                <ul class="pricing-features">
+                  <li class="pricing-features-item">Affordable</li>
+                  <li class="pricing-features-item">3 months</li>
+                </ul>
+                <span class="pricing-price">$0.01</span>
+                <a href="#/" class="pricing-button is-featured">
+                  Pay
+                </a>
+              </div>
 
-function Price () {
-    return(
-     <div>
-      <div class="background1">
-  <div class="container1">
-    <div class="panel pricing-table">
-      
-      <div class="pricing-plan">
-        <img src="images/dollar.png" alt="" class="pricing-img"/>
-        <h2 class="pricing-header">Cheap-ShortTerm</h2>
-        <ul class="pricing-features">
-          <li class="pricing-features-item">Affordable</li>
-          <li class="pricing-features-item">3 months</li>
-        </ul>
-        <span class="pricing-price">$0.01</span>
-        <a href="#/" class="pricing-button is-featured">Pay</a>
+              <div class="pricing-plan">
+                <img src="images/doubledollar.png" alt="" class="pricing-img" />
+                <h2 class="pricing-header">Costly-LongTerm</h2>
+                <ul class="pricing-features">
+                  <li class="pricing-features-item">Costly</li>
+                  <li class="pricing-features-item">6 months</li>
+                </ul>
+                <span class="pricing-price">$0.019</span>
+                <a href="#/" class="pricing-button is-featured">
+                  Pay
+                </a>
+                <div className="paypal-button-container">
+                  <PayPalButtons />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      
-      <div class="pricing-plan">
-        <img src="images/doubledollar.png" alt="" class="pricing-img"/>
-        <h2 class="pricing-header">Costly-LongTerm</h2>
-        <ul class="pricing-features">
-          <li class="pricing-features-item">Costly</li>
-          <li class="pricing-features-item">6 months</li>
-        </ul>
-        <span class="pricing-price">$0.019</span>
-        <a href="#/" class="pricing-button is-featured">Pay</a>
-      </div>
-      
- 
-    </div>
-  </div>
-</div>
-     </div>   
-    )
+    </PayPalScriptProvider>
+  );
 }
 
-export default Price
+export default Price;
