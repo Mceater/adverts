b7356d6171511a17b63aee437ad2c4fec9fbd0d2
Munkh-Batchuluun <munkh.bc@gmail.com>
2022-10-28 12:39:50 +1100

    adform

--- server/index.js
+++ server/index.js
@@ -1,100 +1,78 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const Ad = require("./data-connection");
-const jwks = require("jwks-rsa");
-const jwt = require("express-jwt");
-const authConfig = process.env;
-const multer = require("multer");
-const fs = require("fs");
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const Ad = require('./data-connection')
+const jwks = require("jwks-rsa")
+const jwt = require('express-jwt')
+const authConfig = process.env
+const multer = require('multer')
+const fs = require('fs')
 
-const upload = multer({ dest: "./public/uploads" });
-
-const rawData = fs.readFileSync("server/data.json");
-const data = JSON.parse(rawData);
+const upload = multer({ dest: './public/uploads'})
 
 const checkJWT = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${authConfig.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`,
-  }),
-  audience: `https://${authConfig.REACT_APP_AUDIENCE}`,
-  issuer: `https://${authConfig.REACT_APP_AUTH0_DOMAIN}`,
-  algorithms: ["RS256"],
-});
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${authConfig.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+    audience: authConfig.REACT_APP_AUDIENCE,
+    issuer: `https://${authConfig.REACT_APP_AUTH0_DOMAIN}/`,
+    algorithms: ["RS256"]
+})
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use = express.static("build");
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use = (express.static('build'))
 
-app.post("/api/ads", upload.single("adPhoto"), (req, res, next) => {
-  let fileName = req.file.originalname;
-  fs.rename(
-    `./public/uploads/${req.file.filename}`,
-    "./public/uploads/" + fileName,
-    function () {}
-  );
+app.get('/api/ads', (req, res) => {
+    Ad.find({}).then(result => {
+        res.json(result)
+    })
+})
 
-  const body = req.body;
+app.get('/api/ads/:id', (req, res) => {
+    const id = req.params.id
 
-  const newAd = new Ad({
-    userId: body.userId,
-    img: fileName,
-    url: body.url,
-    endDate: body.endDate,
-    categories: body.categories,
-  });
-  newAd.save().then((result) => {
-    res.json(result.toJSON);
-    console.log("like record saved");
-  });
-  res.json(newAd).catch((err) => next(err));
-});
+    Ad.findById(id).then(result => {
+        res.json(result)
+    })
+})
 
-app.get("/api/ads", (req, res) => {
-  Ad.find({}).then((result) => {
-    res.json(result);
-  });
-});
+app.post ('/api/ads', upload.single("adPhoto"), checkJWT, async (req, res, next) => {
+    let fileName = req.file.originalname
+    fs.rename(`./public/uploads/${req.file.filename}`, "./public/uploads/" + fileName, function(){})
+    console.log(req.file)
 
-app.get("/api/ads/:id", (req, res) => {
-  const id = req.params.id;
-
-  Ad.findById(id).then((result) => {
-    res.json(result);
-  });
-});
-
-/*app.post('/api/ads', checkJWT, async (req, res, next) => {
     const body = req.body
 
     const newAd = new Ad ({
         userId: body.userId,
-        img: body.img,
+        img: fileName,
         url: body.url,
+        startDate: body.startDate,
         endDate: body.endDate,
         categories: body.categories
     })
     newAd.save().then(result => {
         res.json(result.toJSON)
-        console.log("like record saved")
+        console.log("Ad record saved")
     })
-    .catch(err => next(error))
+    .catch(err => next(err))
 })
-*/
 
-app.delete("/api/ads/:id", (req, res, next) => {
-  Ad.findByIdAndRemove(req.params.id)
-    .then((result) => {
-      res.status(204).end();
+app.delete('/api/ads/:id', (req, res, next) => {
+    Ad.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch(error => next(error)) 
+})
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+const PORT = 3001
+app.listen (PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
+
