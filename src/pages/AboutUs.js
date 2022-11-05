8e68641b128611898faee5b08705a2091337ed5c
mceater <farhanrisan@gmail.com>
2022-11-06 02:33:22 +1100

    added about section for farhan

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,16 @@ import Member from "../components/Member";
 
 function AboutUs() {
   const aboutMunkh =
-    "Proud member of GROUP K. Main role in Group K is UI. Tried my best to make sure our web application looks GOOOOD. Alsway welcome to deligthful feedbacks and hates criticism (be careful and have fun).";
+    "Proud member of GROUP K. Main role in Group K is UI. Tried my best to make sure our web application looks GOOOOD. Always welcome to deligthful feedbacks and hates criticism (be careful and have fun).";
   const aboutYeomin =
-    "It was a good experiece!! I have done anything that I can do such as bug fix, merging branches, PayPal implementation, URL Shortner.";
+    "It was a good experience!! I have done anything that I can do such as bug fix, merging branches, PayPal implementation, URL Shortner.";
 
   const aboutShane =
     "Bit of this bit of that, Main role in Group K is to create filter systems and provide documentation";
+
+  const aboutFarhan =
+    "More of a Curious George and mad doctor of the team, helped with implementation of the API used i.e Auth0 , Paypal also added a few design components from Navbar (not anymore present , only in code) to dropdown component to adding style components.";
+
   return (
     <div className="home">
       <div className="about-us-wrapper">
@@ -15,6 +19,7 @@ function AboutUs() {
           name="Farhan Ur Rashid Risan"
           SID="46119108"
           image="munkh.jpg"
+          about={aboutFarhan}
         />
         <Member
           name="Munkh-Erdene"
