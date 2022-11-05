cb04a57d319ecb98ec705201d5ea311860b352ce
sjg2020 <shane.garcia@students.mq.edu.au>
2022-11-05 23:32:32 +1100

    Update to Documentation and about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,12 +4,13 @@ function AboutUs (){
 
     const aboutMunkh = "Proud member of GROUP K. Main role in Group K is UI. Tried my best to make sure our web application looks GOOOOD. Alsway welcome to deligthful feedbacks and hates criticism (be careful and have fun)."
     
+    const aboutShane = "Bit of this bit of that, Main role in Group K is to create filter systems and provide documentation"
     return (
         <div className='home'>
             <div className="about-us-wrapper">
                  <Member name="Farhan Ur Rashid Risan" SID="46119108" image="munkh.jpg" />
                  <Member name="Munkh-Erdene" SID="46119108" image="munkh.jpg" about={aboutMunkh}/>
-                 <Member name="Shane Garcia" SID="46119108" image="munkh.jpg" />
+                 <Member name="Shane Garcia" SID="45415544" image="munkh.jpg" about={aboutShane} />
                  <Member name="Yeo Min Yoon" SID="46119108" image="munkh.jpg" />
             </div>
            
