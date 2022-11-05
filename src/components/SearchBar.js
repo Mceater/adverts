9c1c27e8bf587824d963895ae69f2415e8b7375e
sjg2020 <shane.garcia@students.mq.edu.au>
2022-11-05 20:33:49 +1100

    Update With Popular Categories

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,14 +8,13 @@ function SearchBar({ data, setSearch }) {
   
   const [filteredData, setFilteredData] = useState([]);
   const [searchWord, setSearchWord] = useState("");
+  const [seeSearch, setSeeSearch] = useState(true)
   
 
   var colors = ['#FFEFD5', '#FFF1D0', '#FFF0B7'];
 
   const handleFilter = async (event) => {
-    
-
-   
+    setSeeSearch(true)
     setSearchWord( event.target.value)
     const searchWord = event.target.value;
     setSearch(searchWord)
@@ -55,6 +54,59 @@ function SearchBar({ data, setSearch }) {
 
 }
 
+  
+    
+const handlePopular= () =>{
+
+
+  const newFilter = data.map((element) =>{
+      return element.categories.map((element) =>{
+              return element
+            })
+        })
+
+  const amount = 0;
+  const sortToOne = [];
+  console.log(newFilter)
+  for(let i= 0; i < newFilter.length; i++){
+      for(let j=0; j< newFilter[i].length; j++){
+          const newObject = {
+              category:newFilter[i][j],
+              amount : amount
+          }
+          sortToOne.push(newObject)
+      }
+  }
+
+  let tmp4 = [];
+  sortToOne.map((o) => {
+      console.log(o)
+      const existing = tmp4.find(e => e.category === o.category);
+  if (existing) {
+      existing.amount += 1;
+      return true;
+  } else {
+      tmp4.push({category: o.category, amount: o.amount});
+      return false;
+  }
+  });
+
+  const final = tmp4.filter((element)=>{
+      console.log(element.amount)
+      if(element.amount === 0)
+      {
+          return undefined
+      }
+      else{
+          return element
+      }
+  })
+  setSeeSearch(false)
+  setSearch("")
+  setFilteredData(final)
+
+}
+
 
  const changeInput = (event) =>{
  
@@ -63,14 +115,28 @@ function SearchBar({ data, setSearch }) {
 
  }
 
+ const handleClick =(event) =>{
+  console.log(event.target.value)
+  setSearch(event.target.value);
+}
+
+
   
   return (
 
-    <div className="search">
+      <>
         <div className="searchInputs">
-              <input type="text" placeholder="Search For Categories" value={searchWord} onChange={handleFilter}/>    
+              {seeSearch &&
+              <input type="text" placeholder="Categories..." value={searchWord} onChange={handleFilter}/>    
+              }
+              {
+                seeSearch && <button id="cbutton"onClick={handlePopular}>PopularCategories</button>
+              }
+              {
+                seeSearch || <button id="dbutton"onClick={handleFilter}>Search Categories</button>
+              }
         </div>
-        {filteredData.length !== 0 && (
+        {filteredData.length !== 0 && seeSearch && (
         <div className="dataResult">
             {filteredData.map((value, key)=>{
                return(
@@ -81,7 +147,18 @@ function SearchBar({ data, setSearch }) {
             })}
         </div>
         )}
-    </div>
+        {filteredData.length !== 0 && (seeSearch ||  (
+        <div className="dataResult">
+            {filteredData.map((value, key)=>{          
+               return(
+                <div id="order" key={key}>
+                  <button id="datacategory" onClick={handleClick} style={{background: colors[Math.floor(Math.random() * colors.length)]}} value={value.category}>{value.category},Popularity:{value.amount}</button>
+               </div>
+              );
+            })}
+        </div>
+        ))}
+   </>
   );
 
 }
